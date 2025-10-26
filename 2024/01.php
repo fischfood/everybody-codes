97a6ecf49a97a3551a8cd12dd53d1a421b1c2743
Brian Fischer <brian@linchpin.com>
2025-10-26 11:28:28 -0400

    EC(2024): Quest 1 - Complete

---
+++ 2024/01.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Quest 1: The Battle for the Farmlands
+ ** Part 1: 0.00035 Seconds (4m15s)
+ ** Part 2: 0.00053 Seconds (12m07s)
+ ** Part 3: 0.00116 Seconds (16m00s)
+ */
+
+// The usual
+$starttime = microtime(true);
+$sample_ext = '.txt';
+$sample_ext = '-sample.txt';
+
+$data_1 = file_get_contents('data/01/data-01' . $sample_ext );
+$data_2 = file_get_contents('data/01/data-02' . $sample_ext );
+$data_3 = file_get_contents('data/01/data-03' . $sample_ext );
+
+$dataset_1 = str_split($data_1, 1);
+$dataset_2 = str_split($data_2, 2);
+$dataset_3 = str_split($data_3, 3);
+
+include_once( '../functions.php' );
+
+// Part One
+function part_one( $dataset ) {
+
+	$health = [
+		'A' => 0,
+		'B' => 1,
+		'C' => 3,
+	];
+
+	$totalPotions = 0;
+
+	foreach( $dataset as $creature ) {
+		$totalPotions += $health[$creature];
+	}
+
+	echo $totalPotions;
+
+}
+
+// Part Two
+function part_two( $dataset ) {
+
+	$health = [
+		'A' => 0,
+		'B' => 1,
+		'C' => 3,
+		'D' => 5,
+		'x' => 0,
+	];
+
+	$totalPotions = 0;
+
+	foreach( $dataset as $group ) {
+		$creatures = str_split( $group, 1 );
+
+		foreach( $creatures as $creature ) {
+			$totalPotions += $health[$creature];
+		}
+
+		// Additional potions needed for dual battle (one per creature)
+		if ( ! str_contains( $group, 'x' ) ) {
+			$totalPotions += 2;
+		}
+
+	}
+
+	echo $totalPotions;
+}
+
+// Part Three
+function part_three( $dataset ) {
+
+	$health = [
+		'A' => 0,
+		'B' => 1,
+		'C' => 3,
+		'D' => 5,
+		'x' => 0,
+	];
+
+	$totalPotions = 0;
+
+	foreach( $dataset as $group ) {
+		$creatures = str_split( $group, 1 );
+
+		foreach( $creatures as $creature ) {
+			$totalPotions += $health[$creature];
+		}
+
+		// Remove blank battle spots
+		$opp = strlen( str_replace('x', '', $group ) );
+
+		// If two opponents, add two (one per creature)
+		if ( $opp === 2 ) {
+			$totalPotions += 2;
+		}
+
+		// If three opponents, add six (two per creature)
+		if ( $opp === 3 ) {
+			$totalPotions += 6;
+		}
+
+	}
+
+	echo $totalPotions;
+
+}
+
+echo PHP_EOL . 'Quest 1: The Battle for the Farmlands';
+echo PHP_EOL . 'Part 1: ';
+part_one( $dataset_1 );
+echo PHP_EOL . 'Part 2: ';
+part_two( $dataset_2 );
+echo PHP_EOL . 'Part 3: ';
+part_three( $dataset_3 );
+echo PHP_EOL;
+echo 'Total time to generate: ' . ( microtime( true ) - $starttime );
+echo PHP_EOL;
